397838d31483f90d08700efbdb68ce144d5d2f4f
Kajetan Bojko <kai@shutl.co.uk>
2014-08-24 15:06:34 +0100

    fixed problem with distance calculation erratic behaviour due to interference between successive pings

--- compass/compass.ino
+++ compass/compass.ino
@@ -24,8 +24,10 @@ DRDY      N/A
 
 #define RAD_TO_DEG 57.2957795 
 
-#define trigPin A2
-#define echoPin A3
+#define TRIG_PIN_RIGHT A0
+#define ECHO_PIN_RIGHT A1
+#define TRIG_PIN_LEFT A2
+#define ECHO_PIN_LEFT A3
 
 #define INFINITE_DISTANCE 32767
 
@@ -49,8 +51,10 @@ AF_DCMotor motorR(2);
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(TRIG_PIN_LEFT, OUTPUT);
+  pinMode(ECHO_PIN_LEFT, INPUT);
+  pinMode(TRIG_PIN_RIGHT, OUTPUT);
+  pinMode(ECHO_PIN_RIGHT, INPUT);
 
   Init_HMC5803L();
 
@@ -267,7 +271,7 @@ int getAngle() {
   return round(RAD_TO_DEG * atan2(x, y));
 }
 
-void ping() {
+void ping(int trigPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -279,40 +283,48 @@ boolean isInfinite(int distance) {
   return distance == INFINITE_DISTANCE;
 }
 
+int pingDurationToMm(int duration) {
+  if (duration == 0) {
+    return INFINITE_DISTANCE;
+  } else {
+    return (duration/2) / 2.91;
+  };
+}
+
+unsigned long getPingDuration(int trigPin, int echoPin) {
+  ping(trigPin);
+  return pulseIn(echoPin, HIGH);
+}
+
 int getDistanceInMm() {
-  unsigned long avgDuration;
-  unsigned long sumDuration = 0;
+  int sumDistance = 0;
   int goodSampleCount = 0;
 
   for(int i=0; i<RANGE_FINDER_REPEAT; i++) {
-    ping();
-    unsigned long duration = pulseIn(echoPin, HIGH);
-    if (duration > 0) {
-      sumDuration += duration;
+    int distance = 
+      pingDurationToMm(getPingDuration(TRIG_PIN_LEFT, ECHO_PIN_LEFT));
+
+    if (!isInfinite(distance)) {
+      sumDistance += distance;
       goodSampleCount++;
     }
+    delay(5); // we have to delay otherwise pings interfere with each other
   }
 
   if (goodSampleCount == 0) { return INFINITE_DISTANCE; };
-
-  avgDuration = round((float)sumDuration / goodSampleCount);
-
-  if (avgDuration == 0) {
-    return INFINITE_DISTANCE;
-  } else {
-    return (avgDuration/2) / 2.91;
-  };
+  return round((float) sumDistance / (float) goodSampleCount);
 }
 
 void loop() 
 {
-  int angle = scanForBestDirection(360);
-  rotateTo(angle, rotateLeft, rotateRight);
-  goSlowlyToObstacle(angle);
+ printVal("getDistanceInMm() = ", getDistanceInMm());
+
+  // int angle = scanForBestDirection(360);
+  // rotateTo(angle, rotateLeft, rotateRight);
+  // goSlowlyToObstacle(angle);
 
   // scanForBestDirection(90);
   // rotateBy(-90, rotateLeftForward, rotateRightForward);
 
   // goToObstacle(getAngle());
-  // delay(1000);
 }
