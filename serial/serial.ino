6a405fed6ce9f597ceea9b6b10e2921cf6551f4b
Kajetan Bojko <kai@shutl.co.uk>
2014-10-14 22:26:46 +0100

    serial port with some diagnostic blinking

--- serial/serial.ino
+++ serial/serial.ino
@@ -1,13 +1,26 @@
 void setup() {
   Serial.begin(9600);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
 char c;
 
+void blink(int count) {
+  for (int i=0; i<count; i++) {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100);
+  }
+}
+
 void loop() {
   if (Serial.available() > 0) {
     c = Serial.read();
-    Serial.println(c);
+    Serial.println(String(c));
     Serial.flush();
   }
+  // delay(100);
 }
