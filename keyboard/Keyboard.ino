714f3a76beeb464138babaf1a77bd47373ea9255
Kajetan Bojko <kai@shutl.co.uk>
2015-08-13 22:44:44 +0100

    one button keyboard ;)

--- keyboard/Keyboard.ino
+++ keyboard/Keyboard.ino
@@ -1,10 +1,60 @@
+/*
+ The circuit:
+ * pushbutton attached from pin 4 to +5V
+ * 10-kilohm resistor attached from pin 4 to ground
+ */
+
+const int buttonPin = 2;
+int previousButtonState = LOW;
+int buttonState = LOW;
+
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 10;
+
+unsigned long triggerTime = 0;
+boolean autoRepeat = false;
+const unsigned long autoRepeatDelay = 600;
+const unsigned long autoRepeatTime = 100;
+
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Keyboard.begin();
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(100);
-  digitalWrite(13, LOW);
-  delay(100);
+  int reading = digitalRead(buttonPin);
+
+  if (reading != previousButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      if ((reading == HIGH) && (buttonState == LOW)) {
+        Keyboard.print("a");
+        triggerTime = millis();
+        autoRepeat = false;
+      }
+
+      buttonState = reading;
+    }
+
+    if ((reading == HIGH) && (buttonState == HIGH)) {
+      if (autoRepeat) {
+        if ((millis() - triggerTime) > autoRepeatTime) {
+          Keyboard.print("a");
+          triggerTime = millis();
+        }
+      } else {
+        if ((millis() - triggerTime) > autoRepeatDelay) {
+          Keyboard.print("a");
+          triggerTime = millis();
+          autoRepeat = true;
+        }
+      }
+    }
+  }
+
+  previousButtonState = reading;
 }
+
